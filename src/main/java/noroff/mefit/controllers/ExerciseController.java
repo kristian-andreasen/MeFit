479042a1118d8a74bb990fbe560f8e5833031982
Asbiorn Muller <asbiorn@gmail.com>
2023-03-24 10:05:36 +0100

    adding try catch and api responses for get and getAll

--- src/main/java/noroff/mefit/controllers/ExerciseController.java
+++ src/main/java/noroff/mefit/controllers/ExerciseController.java
@@ -1,12 +1,20 @@
 package noroff.mefit.controllers;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import noroff.mefit.dtos.ExerciseDTO;
 import noroff.mefit.mappers.ExerciseMapper;
 
 import noroff.mefit.models.Exercise;
 import noroff.mefit.services.ExerciseService;
 import noroff.mefit.services.ExerciseServiceImpl;
+import noroff.mefit.util.ApiErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,34 +31,56 @@ public class ExerciseController {
         this.exerciseService = exerciseService;
         this.exerciseMapper = exerciseMapper;
     }
-
+    @Operation(summary = "Get all exercises")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = ExerciseDTO.class))) }),
+            @ApiResponse(responseCode = "404",
+                    description = "Exercise does not exist with supplied ID",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiErrorResponse.class)) })
+    })
     @CrossOrigin(origins = "http://localhost:3000")
-/*    @GetMapping("")
-    public ResponseEntity<Collection<ExerciseDTO>> getAll(){
-        Collection<Exercise> exercises = exerciseService.findAll();
-        Collection<ExerciseDTO> exerciseDTOs = exercises.stream()
-                .map(exerciseMapper::exerciseDTO)
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok().body(exerciseDTOs);
-    }*/
-
-    @GetMapping // GET: localhost:8080/api/v1/exercies
+    @GetMapping("")
     public ResponseEntity getAll() {
-        Collection<ExerciseDTO> studs = exerciseMapper.exercisesToExercisesDto(
-                exerciseService.findAll()
-        );
-        return ResponseEntity.ok(studs);
+        try {
+            Collection<ExerciseDTO> exerciseDTOs = exerciseMapper.exercisesToExercisesDto(
+                    exerciseService.findAll()
+            );
+            return ResponseEntity.ok(exerciseDTOs);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
+
+    @Operation(summary = "Get a exercise by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Success",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ExerciseDTO.class)) }),
+            @ApiResponse(responseCode = "404",
+                    description = "Exercise does not exist with supplied ID",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiErrorResponse.class)) })
+    })
     @GetMapping("{id}")
     public ResponseEntity getById(@PathVariable int id) {
-
-        ExerciseDTO exerciseDTO = exerciseMapper.exerciseDTO(exerciseService.findById(id));
-
-        return ResponseEntity.ok(exerciseDTO);
+        try {
+            ExerciseDTO exerciseDTO = exerciseMapper.exerciseDTO(exerciseService.findById(id)
+            );
+            return ResponseEntity.ok(exerciseDTO);
+    } catch (Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
-
+}
 
     @PostMapping()
     public ResponseEntity<Exercise> add(@RequestBody ExerciseDTO exerciseDTO) {
@@ -62,17 +92,15 @@ public class ExerciseController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<ExerciseDTO> update(@PathVariable("id") int id, @RequestBody ExerciseDTO exerciseDTO) {
+    public ResponseEntity<ExerciseDTO> update(@PathVariable int id, @RequestBody ExerciseDTO exerciseDTO) {
     Exercise existingExercise = exerciseService.findById(id);
-    if (existingExercise == null) {
-        return ResponseEntity.notFound().build();
+    if (id != exerciseService.findById(id).getId()) {
+        return ResponseEntity.badRequest().build();
     }
     exerciseMapper.updateExerciseFromDto(exerciseDTO, existingExercise);
+        System.out.println(existingExercise.getSetCounts());
     exerciseService.update(existingExercise);
-    // not strictly necessary to return the updated DTO
-    ExerciseDTO updatedDto = exerciseMapper.exerciseDTO(existingExercise);
-    return ResponseEntity.ok(updatedDto);
-
+    return ResponseEntity.ok(exerciseMapper.exerciseDTO(existingExercise));
 
 }
 
